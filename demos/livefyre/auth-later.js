3e4e8c16784718e7a3c1c376c4aa937e34e2d796
JJ Weber <jjweber@gmail.com>
2014-07-02 14:21:52 -0700

    Add auth later for wu wei auth loading

---
+++ demos/livefyre/auth-later.js
@@ -0,0 +1,19 @@
+var auth = require('livefyre-auth/contrib/auth-later');
+require('livefyre-auth').plugin(auth);
+
+var log = require('debug')('livefyre-auth-demo');
+var createAuthButton = require('auth/contrib/auth-button');
+var createAuthLog = require('auth/contrib/auth-log');
+var livefyreDelegate = require('livefyre-auth/livefyre-auth-delegate');
+
+var delegate = window.delegate = livefyreDelegate('http://www.livefyre.com');
+auth.delegate(delegate);
+
+auth.on('login.livefyre', function (livefyreUser) {
+    log("User was logged into Livefyre", livefyreUser);
+});
+
+window.auth = auth;
+
+var authLog = createAuthLog(auth, document.getElementById('auth-log'));
+createAuthButton(auth, document.getElementById('auth-button'), authLog);
